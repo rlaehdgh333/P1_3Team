28c177170315fc9678b1275864674822b2790bc4
minji son <sc2580@hanmail.net>
2016-02-03 16:37:58 +0900

    help.java add contents

--- P1_3Team/src/com/sist/Help.java
+++ P1_3Team/src/com/sist/Help.java
@@ -1,10 +1,13 @@
 package com.sist;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Help {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Help();
 	}
 
 }
